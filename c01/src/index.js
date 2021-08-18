932d9dfc840a99e9edf73152d0262585442f0513
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-08-17 22:34:19 -0300

    f

--- c01/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Brazilian_1 = require("./Brazilian");
-const eu = new Brazilian_1.Brazilian("Gustavo", "Schneider", 22, "000.000.000-00");
-console.table(eu);
--- c01/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Brazilian } from "./Brazilian";
-
-const eu = new Brazilian("Gustavo", "Schneider", 22, "000.000.000-00");
-
-console.table(eu);
